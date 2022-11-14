e82a12524c52652c0bbaf77656538d9f950c0c5a
Vääräsmäki <mvaaras@gmail.com>
2022-11-14 13:59:12 +0200

    Tehtävä 3 viikko 2

--- src/varasto.py
+++ src/varasto.py
@@ -1,22 +1,17 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
+        self.tilavuus = self.validate(tilavuus,tilavuus)
+        self.saldo = self.validate(alku_saldo, tilavuus)
 
-        if alku_saldo < 0.0:
-            # virheellinen, nollataan
-            self.saldo = 0.0
-        elif alku_saldo <= tilavuus:
-            # mahtuu
-            self.saldo = alku_saldo
-        else:
-            # täyteen ja ylimäärä hukkaan!
-            self.saldo = tilavuus
+    # huom: ominaisuus voidaan myös laskea.
+    # Ei tarvita erillistä kenttää viela_tilaa tms.
+    def validate(self,value,max_value):
+        if value >= 0:
+            if value <= max_value:
+                return value
+            return max_value
+        return 0
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
